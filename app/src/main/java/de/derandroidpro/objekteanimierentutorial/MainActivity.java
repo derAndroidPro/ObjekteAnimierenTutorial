ad2d41a37ff23941d9649617977a9472758e3854
derAndroidPro <der.android.david@gmail.com>
2015-05-02 14:34:24 +0200

    Initial commit

---
+++ app/src/main/java/de/derandroidpro/objekteanimierentutorial/MainActivity.java
@@ -0,0 +1,118 @@
+package de.derandroidpro.objekteanimierentutorial;
+
+import android.animation.Animator;
+import android.os.Build;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    ImageView iv;
+    Button btn;
+     int counter;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        iv = (ImageView) findViewById(R.id.imageView);
+        btn = (Button) findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                counter ++;
+
+                if((counter % 2)== 1){
+                    if(Build.VERSION.SDK_INT >=12) {
+                        iv.animate().translationX(iv.getRight()).rotation(360).alpha(0).setDuration(300).setListener(new Animator.AnimatorListener() {
+                            @Override
+                            public void onAnimationStart(Animator animation) {
+
+                            }
+
+                            @Override
+                            public void onAnimationEnd(Animator animation) {
+                                btn.setText("Objekt einblenden");
+                            }
+
+                            @Override
+                            public void onAnimationCancel(Animator animation) {
+
+                            }
+
+                            @Override
+                            public void onAnimationRepeat(Animator animation) {
+
+                            }
+                        });
+                    }   else{
+                        iv.setVisibility(View.INVISIBLE);
+                    }
+                }
+
+                if((counter % 2)== 0){
+
+                    if (Build.VERSION.SDK_INT >= 12) {
+                        iv.animate().translationX(0).rotation(0).alpha(1).setDuration(300).setListener(new Animator.AnimatorListener() {
+                            @Override
+                            public void onAnimationStart(Animator animation) {
+
+                            }
+
+                            @Override
+                            public void onAnimationEnd(Animator animation) {
+                                btn.setText("Objekt ausblenden");
+                            }
+
+                            @Override
+                            public void onAnimationCancel(Animator animation) {
+
+                            }
+
+                            @Override
+                            public void onAnimationRepeat(Animator animation) {
+
+                            }
+                        });
+                    } else {
+                        iv.setVisibility(View.VISIBLE
+                        );
+                    }
+                }
+
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
